ebe61047f2287eccffe138dac56179118cf50f85
Marcio202302986072 <142801419+Marcio202302986072@users.noreply.github.com>
2024-06-12 23:35:38 -0300

    Delete projeto luna/Raylib-CPP-Starter-Template-for-VSCODE-V2-main directory

--- projeto luna/Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/mang.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "vilao.hpp"
-
-class Mang : public Vilao {
-public:
-    Mang(int type, Vector2 position);
-};
--- projeto luna/Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/tiro.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "laser.hpp"
-
-class Tiro : public Laser {
-public:
-    Tiro(Vector2 position, int speed);
-};
